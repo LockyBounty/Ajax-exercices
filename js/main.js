1195669e45da79a994b802929b8a4cae282e71f5
Locky <inox127@hotmail.com>
2018-11-19 15:34:15 +0100

    added lines

--- js/main.js
+++ js/main.js
@@ -1,23 +1,32 @@
-{   
+// {   
 
-    const createResultList = (results) => {
+//     const createResultList = (results) => {
  
-    }
+//     }
 
-    const search = value => {
-        const url = `https://musicdemons.com/api/v1/artist/autocomplete`;
+//     const search = value => {
+//         const url = `https://musicdemons.com/api/v1/artist/autocomplete`;
 
-    };
+//     };
 
-    const handleKeyUpSearch = e => {
-        const $input = e.currentTarget;
-        search($input.value);
-    };
+//     const handleKeyUpSearch = e => {
+//         const $input = e.currentTarget;
+//         search($input.value);
+//     };
 
-    const init = () => {
-        document.querySelector(`.search`).addEventListener(`keyup`, handleKeyUpSearch);
-    };
+//     const init = () => {
+//         document.querySelector(`.search`).addEventListener(`keyup`, handleKeyUpSearch);
+//     };
 
-    init();
+//     init();
+
+//}
+
+let xhr= new XMLHttpRequest();
+xhr.open(`POST`, `https://musicdemons.com/api/v1/artist/autocomplete`);
+
+xhr.onload= function(){
+    console.log(xhr.responseText);
 
 }
+xhr.send();

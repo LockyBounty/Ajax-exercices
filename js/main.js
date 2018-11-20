403b04a0f43d008a07c654dcd2eea6a053fc4ec9
Locky <inox127@hotmail.com>
2018-11-20 15:12:35 +0100

    added lines - fetch to do

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // {   
 
 //     const createResultList = (results) => {
- 
+
 //     }
 
 //     const search = value => {
@@ -22,14 +22,41 @@
 
 //}
 
-let hunter = document.querySelector(".results");
+let donneesNSA;
+let hunter = document.querySelector(".content");
+let artist = document.querySelector(".artist");
+let zoneSong = document.querySelector(".songs");
+
+let el87;
 
 
-let xhr= new XMLHttpRequest();
+let xhr = new XMLHttpRequest();
 xhr.open(`POST`, `https://musicdemons.com/api/v1/artist/autocomplete`);
 
-xhr.onload= function(){
-    console.log(xhr.responseText);
+xhr.onload = function () {
+    if (this.status === 200) {
+        console.log(xhr.responseText);
+        donneesNSA = JSON.parse(xhr.responseText);
+    }
+    loadTextLocky();
 
 }
 xhr.send();
+
+/* On crée et on met notre fonction dans une boucle pour générer une liste */
+/* et la fonction est appelée dans le xhr */
+
+
+let loadTextLocky = () => {
+
+    artist.innerHTML = `${donneesNSA[i].name}`;
+
+    
+    el87 = document.createElement("LI");
+    el87.innerHTML = `${donneesNSA[i].text}`;
+    let afficheAuTop87 = zoneSong.appendChild(el87);
+    zoneSong.insertBefore(afficheAuTop87, zoneSong.firstElementChild);
+
+
+}
+
